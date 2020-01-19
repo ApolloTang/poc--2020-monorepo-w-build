b5cc938b242846aa6b603bf2c05e087f346dd5c9
Apollo Tang <tang.apollo@gmail.com>
2020-01-19 03:28:43 -0500

    add component package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-    stories: ['../packages/**/*.stories.js'],
+  stories: ['../components/**/src**/*.stories.js'],
+
     // addons: ['@storybook/addon-docs']
 };
 
---
+++ components/button/src/index.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import Button from '.';
+
+storiesOf('Button-2', module).add('default', () => <Button>{'Button'}</Button>);
