b1f0ca84741ded660bce5a82e2348c6b0ac68ea3
Apollo Tang <tang.apollo@gmail.com>
2020-01-19 02:45:46 -0500

    story book working

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+    stories: ['../packages/**/*.stories.js'],
+    // addons: ['@storybook/addon-docs']
+};
+
+// import {configure} from '@storybook/react';
+//
+//
+// const req = require.context('../packages', true, /.story.js$/);
+//
+// function loadStories() {
+//     req.keys().forEach((filename) => req(filename));
+// }
+//
+// configure(loadStories, module);
+
+
